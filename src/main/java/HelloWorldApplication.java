558069c5ee3b7470c9db96d13bc47761305eb053
ravikiran <ravikirna763@gmail.com>
2025-06-05 11:24:47 +0100

    Added initial commit

---
+++ src/main/java/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class HelloWorldApplication {
+
+
+    public static void main(String[] args) throws Exception {
+       System.out.println("Hello World");
+    }
+
+}
